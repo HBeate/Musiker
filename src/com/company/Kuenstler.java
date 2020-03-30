d8a8b91cb0a30a2d54b620be7060012de7d8c7a5
HBeate <hauser_b@hotmail.com>
2020-03-30 16:26:25 +0200

    Fertig???

--- src/com/company/Kuenstler.java
+++ src/com/company/Kuenstler.java
@@ -3,8 +3,16 @@ package com.company;
 public class Kuenstler extends Person {
     private String kuenstlerName;
 
-    public Kuenstler(String kuenstlerName, String name, String vorname){
+    public Kuenstler(String kuenstlerName, String name, String vorname) {
         super(name, vorname);
+        this.kuenstlerName = kuenstlerName;
+    }
+
+    public String getKuenstlerName() {
+        return kuenstlerName;
+    }
+
+    public void setKuenstlerName(String kuenstlerName) {
         this.kuenstlerName = kuenstlerName;
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -5,10 +5,36 @@ public class Person {
     private String vorname;
     private int alter;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
     public Person(String name, String vorname) {
         super();
         this.name = name;
         this.vorname = vorname;
+
+
     }
 }
 
